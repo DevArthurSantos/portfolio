ca4057ff10ee524b5e3496dae61aee92c65bf191
DevArthurSantos <arthursantos.s@hotmail.com>
2023-03-08 04:44:03 -0300

    feat: current template mode save option

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,41 @@
 //Init tooltips
-tippy('.link', {
-  placement: 'bottom'
-})
+tippy(".link", {
+  placement: "bottom",
+});
 
 //Toggle mode
-const toggle = document.querySelector('.js-change-theme');
-const body = document.querySelector('body');
-const profile = document.getElementById('profile');
+const toggle = document.querySelector(".js-change-theme");
+const body = document.querySelector("body");
+const profile = document.getElementById("profile");
 
+function altTemplaterModeTotoBlack() {
+  toggle.innerHTML = "🌙";
+  body.classList.remove("text-gray-100");
+  body.classList.add("text-gray-900");
+  profile.classList.remove("bg-gray-900");
+  profile.classList.add("bg-white");
+  localStorage.setItem("templateMode", "toBlack");
+}
+function altTemplaterModeTotoWhite() {
+  toggle.innerHTML = "☀️";
+  body.classList.remove("text-gray-900");
+  body.classList.add("text-gray-100");
+  profile.classList.remove("bg-white");
+  profile.classList.add("bg-gray-900");
+  localStorage.setItem("templateMode", "toWhite");
+}
 
-toggle.addEventListener('click', () => {
-
-  if (body.classList.contains('text-gray-900')) {
-    toggle.innerHTML = "☀️";
-    body.classList.remove('text-gray-900');
-    body.classList.add('text-gray-100');
-    profile.classList.remove('bg-white');
-    profile.classList.add('bg-gray-900');
+function checkCurrentTemplate(mode){
+  if (mode === "toWhite") {
+    altTemplaterModeTotoBlack();
   } else {
-    toggle.innerHTML = "🌙";
-    body.classList.remove('text-gray-100');
-    body.classList.add('text-gray-900');
-    profile.classList.remove('bg-gray-900');
-    profile.classList.add('bg-white');
-
+    altTemplaterModeTotoWhite();
   }
+}
+
+if (localStorage.getItem("templateMode") === "toWhite") altTemplaterModeTotoWhite()
+if (!localStorage.getItem("templateMode") || localStorage.getItem("templateMode") === "toBlack") altTemplaterModeTotoBlack();
+
+toggle.addEventListener("click", () => {
+  checkCurrentTemplate(localStorage.getItem("templateMode"))
 });
